adfa9a6887a08d8c23d256f8d0b74c2cbdd4a2ba
Callie Nipper <callie.nipper@gmail.com>
2021-04-12 18:31:55 -0700

    Added Foreign Keys to company starter file

--- db/company_starter.sql
+++ db/company_starter.sql
@@ -15,7 +15,7 @@ title VARCHAR(30) NOT NULL,
 salary DECIMAL (10, 4) NOT NULL,
 dept_id INT NOT NULL,
 PRIMARY KEY(id),
-FOREIGN KEY ()
+FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -24,5 +24,7 @@ first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT NOT NULL,
 manager_id INT,
-PRIMARY KEY(id)
+PRIMARY KEY(id),
+FOREIGN KEY (role_id) REFERENCES role (id),
+FOREIGN KEY (manager_id) REFERENCES role (id)
 );
