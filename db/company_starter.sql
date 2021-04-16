baed666b50ac8d5d306df6c4a98665b97927ecb3
Callie Nipper <callie.nipper@gmail.com>
2021-04-15 21:39:33 -0700

    Seeded databases

--- db/company_starter.sql
+++ db/company_starter.sql
@@ -47,4 +47,10 @@ INSERT INTO role (title, salary, dept_id ) VALUES
 ('Software Engineer', 75000, 6);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('Elizabeth', 'Bennet', 5, null),
+('Jane', 'Bennet', 2, 1),
+('Charles', 'Bingley', 2, 2),
+('Fitzwilliam', 'Darcy', 6, 1),
+('George', 'Wickham', 1, 4),
+('Charlotte', 'Lucas', 3, 2);
 
